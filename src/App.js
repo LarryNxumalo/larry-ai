0c02c233545eb374a4806772652136b5fb45a67e
Larry Nxumalo <larry.nxumalo@afrihost.com>
2020-08-25 03:17:13 +0200

    alan ai reading and higlighting current articles - have to fix window.open error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 //data
 const alanKey = '79a503e22a811db5e1980c3f389cab3d2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
-//methods
+    //State field
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    //methods
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
-                if(command === 'newHeadlines'){
-                    console.log(articles);
+            onCommand: ({ command, articles, number }) => {
+                if(command === 'newHeadlines') {
+                   setNewsArticles(articles);
+                   setActiveArticle(-1);
+                } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if (command === 'open') {
+                    window.open(articles[number].url, '_blank');
+                    // console.log('number');
                 }
             }
 
@@ -19,9 +30,10 @@ const App = () => {
     }, [])
     return (
         <div>
-            <h1>Larry AI news Application</h1>
+            <h1>Phepha Ndaba</h1>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
         </div>
-    )
+    );
 }
 
 export default App;
